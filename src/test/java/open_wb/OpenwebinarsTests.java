0cb69a776e59349f7cd53c5889871fb53174fa90
plaiaundi <plaiaundi@DESKTOP-TCBF7JG>
2022-11-02 08:39:33 +0100

    lehen eta bigarren testen probak

--- src/test/java/open_wb/OpenwebinarsTests.java
+++ src/test/java/open_wb/OpenwebinarsTests.java
@@ -1,5 +1,19 @@
 package open_wb;
 
-public class OpenwebinarsTests {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+public class OpenwebinarsTests {
+	
+	@Test
+	@DisplayName("Primera prueba junit 5")
+	void primeraPrueba() {
+		System.out.println("Esta es la primera prueba");
+	}
+	
+	@Test
+	@DisplayName("Segunda prueba junit 5")
+	void segundaPrueba() {
+		System.out.println("Esta es la segunda prueba");
+	}
 }
