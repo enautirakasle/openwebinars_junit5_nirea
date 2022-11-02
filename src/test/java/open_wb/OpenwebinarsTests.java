cfb74828be0cff06b93114822898d78ea1ebbd3b
plaiaundi <plaiaundi@DESKTOP-TCBF7JG>
2022-11-02 10:06:40 +0100

    asserttrue, assertequals, assertarrayequelas test probak

--- src/test/java/open_wb/OpenwebinarsTests.java
+++ src/test/java/open_wb/OpenwebinarsTests.java
@@ -1,19 +1,37 @@
 package open_wb;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OpenwebinarsTests {
 	
 	@Test
-	@DisplayName("Primera prueba junit 5")
-	void primeraPrueba() {
-		System.out.println("Esta es la primera prueba");
+	@DisplayName("Aserción TRUE")
+	void pruebaBoolean() {
+		boolean verdadero = true;
+		assertTrue(verdadero);
+
 	}
 	
 	@Test
-	@DisplayName("Segunda prueba junit 5")
-	void segundaPrueba() {
-		System.out.println("Esta es la segunda prueba");
+	@DisplayName("Aserción EQUALS")
+	void pruebaEquals() {
+		String cadenaEsperada = "esperada";
+		String cadenaActual = "esperada";
+		
+		assertEquals(cadenaEsperada, cadenaActual);
+	}
+	
+	@Test
+	@DisplayName("Aserción ARRAY EQUALS")
+	void pruebaArray() {
+		String[] cadenasEsperadas = {"cadena1", "cadena2", "cadena3"};
+		String[] cadenasActuales = {"cadena1",  "cadena3"};
+		
+		assertArrayEquals(cadenasEsperadas, cadenasActuales);
 	}
 }
