3280db4f1acbcf407386992796f336d1d2c990b7
enaut aa <enaut.irakasle@gmail.com>
2022-11-20 19:49:00 +0100

    before all afer all

--- src/test/java/open_wb/SeleniumTests.java
+++ src/test/java/open_wb/SeleniumTests.java
@@ -1,34 +1,55 @@
 package open_wb;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTests {
 
+	public static WebDriver driver;
+	
 	@BeforeAll
 	static void before_all() {
-		System.out.println("Se ejecuta ANTES de todo");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
-	
+
 	@Test
 	@DisplayName("Soy el test 1")
 	void test1() {
-		System.out.println("Soy el test 1");
+		driver.get("http://www.saucedemo.com");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	@Test
 	@DisplayName("Soy el test 2")
 	void test2() {
-		System.out.println("Soy el test 2");
+		driver.get("http://www.wikipedia.com");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	@AfterAll
 	static void after_all() {
-		System.out.println("Se ejecuta DESPUES de todo");
+		if(driver != null) {
+			driver.quit();
+		}
+			
 	}
-	
-	
-	
+
 }
