cdd0e886c16702d9f77a813b03d2c7c3b0b17537
enaut aa <enaut.irakasle@gmail.com>
2022-11-20 20:15:07 +0100

    login standard_user erabiltzailearekin

--- src/main/java/eus/agirrearri/open_wb/SeleniumApp.java
+++ src/main/java/eus/agirrearri/open_wb/SeleniumApp.java
@@ -3,6 +3,7 @@ package eus.agirrearri.open_wb;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,6 +18,15 @@ public class SeleniumApp {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		driver.get("http://www.saucedemo.com");
+				
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		String texto = driver.findElement(By.className("title")).getText();
+		
+		System.out.println(texto);
+		
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
